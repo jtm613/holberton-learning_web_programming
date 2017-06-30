19ab96dfbcebc3258dd158c6dddc899928b4554d
jtm613 <jtmacoco@comcast.net>
2017-06-30 14:42:05 -0700

    reverse and  more arrays

---  5-javascript/102-array.js
+++  5-javascript/102-array.js
@@ -1,2 +1,6 @@
-var dwarf = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey"]
-console.log(dwarf[0])
+var dwarf = ["Doc", "Grumpy", "Happy", "Sleepy", "Bashful", "Sneezy", "Dopey"];
+for(let i = 0; i<=dwarf.length; i++)
+{
+console.log(dwarf[i]);
+
+}
