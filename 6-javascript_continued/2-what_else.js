a0cf2b4734298409525ed8f94efebd45bf6f5306
jtm613 <jtmacoco@comcast.net>
2017-07-03 12:34:18 -0700

    less than 5

---
+++ 6-javascript_continued/2-what_else.js
@@ -0,0 +1,7 @@
+let my_parameter = process.argv[2];
+// Print the my_parameter variable
+if( my_parameter >= 5){
+console.log( my_parameter + " is greater than 5");
+} else{
+  console.log(my_parameter +" is less than 5");
+}
