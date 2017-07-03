a34caf494ae2efb7b494cc30f42e742cc1fe07f0
jtm613 <jtmacoco@comcast.net>
2017-07-03 13:06:41 -0700

    took away spacing

--- 6-javascript_continued/4-clock.js
+++ 6-javascript_continued/4-clock.js
@@ -1,13 +1,13 @@
-var hour = 0;
-var minute = 0;
-while(hour <= 23)
+var hour=0;
+var minute=0;
+while(hour<=23)
 {
-  minute = 0;
-  while(minute <= 59)
+  minute=0;
+  while(minute<=59)
   {
-    console.log(minute)
-      minute++
+    console.log(minute);
+      minute++;
   }
-  hour++
-  console.log(hour)
+  hour++;
+  console.log(hour);
 }
