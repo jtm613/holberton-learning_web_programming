da732171305a62ec75af713e1f8bee763e1eb5d5
jtm613 <jtmacoco@comcast.net>
2017-07-03 12:43:53 -0700

    grading

--- 6-javascript_continued/2-what_else.js
+++ 6-javascript_continued/2-what_else.js
@@ -2,6 +2,7 @@ let my_parameter = process.argv[2];
 // Print the my_parameter variable
 if( my_parameter >= 5){
 console.log( my_parameter + " is greater than 5");
-} else{
+}
+else{
   console.log(my_parameter +" is less than 5");
 }
---
+++ 6-javascript_continued/3-grading_system.js
@@ -0,0 +1,24 @@
+let my_parameter = process.argv[2];
+// Print the my_parameter variable
+if( my_parameter >= 95)
+console.log( my_parameter + " You received an A+");
+else if(my_parameter >= 90)
+{
+  console.log(my_parameter + " YOu received an A")
+}
+else if(my_parameter >= 85)
+{
+  console.log(my_parameter + " YOu received an B+")
+}
+else if(my_parameter >= 80)
+{
+  console.log(my_parameter + " YOu received an B")
+}
+else if(my_parameter >= 75)
+{
+  console.log(my_parameter + " YOu received an C+")
+}
+else if(my_parameter >= 70)
+{
+  console.log(my_parameter + " YOu received an C")
+}
