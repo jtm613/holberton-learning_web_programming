045b9601ddd21c735adccf086efee024319b0ec0
jtm613 <jtmacoco@comcast.net>
2017-07-03 12:47:30 -0700

    took away spacing

--- 6-javascript_continued/2-what_else.js
+++ 6-javascript_continued/2-what_else.js
@@ -1,7 +1,7 @@
 let my_parameter=process.argv[2];
 if( my_parameter>= 5){
-console.log( my_parameter + " is greater than 5");
+console.log( my_parameter+"The value is greater than 5");
 }
 else{
-  console.log(my_parameter +" is less than 5");
+  console.log(my_parameter+"The value is equal to 5 or lower");
 }
