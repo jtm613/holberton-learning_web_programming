c11b82f946325e1b384d04365d52fab5df6fa94a
jtm613 <jtmacoco@comcast.net>
2017-07-03 15:13:07 -0700

    questions

---
+++ 6-javascript_continued/ multiplication_table.js
@@ -0,0 +1,12 @@
+var arr = [];
+for (var r = 0; r < 11; r++) {
+  arr[r] = [];
+  for((var c = 0; c <= 11; c++)
+  {
+    arr[r][c] = (r+1)(c+1);
+  }
+}
+function show()
+{
+    document.getElementById("multi_table")
+}
